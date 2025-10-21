a9711569cde648fbec5d66b7ecf69391585d7157
Rachel Previtali <rachel_previtali@outlook.com>
2025-10-21 11:55:39 -0300

    adicionei um rectangle shape em Entidade

--- Entidade.cpp
+++ Entidade.cpp
@@ -1,10 +1,15 @@
 #include "Entidade.h"
 
-Entidade::Entidade(): veloc(0.0,0.0)
+Entidade::Entidade(): veloc(0.0,0.0), body(sf::Vector2f(10.0,10.0))
 {
 }
 
 Entidade::~Entidade() { }
+
+sf::RectangleShape Entidade::getBody()
+{
+	return body;
+}
 
 void Entidade::executar()
 {
