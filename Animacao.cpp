adb3e3819e5cf9ab335dd2e569fd2fd7b2f9a007
Rachel Previtali <rachel_previtali@outlook.com>
2025-10-22 18:55:58 -0300

    Criação da classe Animação

---
+++ Animacao.cpp
@@ -0,0 +1,43 @@
+#include "Animacao.h"
+
+
+Animacao::Animacao(sf::RectangleShape* body):
+	corpo(body),
+	mapImagens(),
+	clock(),
+	estadoAtual("")
+{
+
+
+}
+
+Animacao::~Animacao() { }
+
+void Animacao::atualizar(const bool indoParaEsq, std::string estadoImg)
+{
+	if (estadoAtual != estadoImg) //caso o personagem esteja em um estado diferente do atual, resetamos o tempo e o "index" da textura
+	{
+		estadoAtual = estadoImg;
+		mapImagens[estadoAtual]->resetar(); 
+	}
+
+	float dt = clock.getElapsedTime().asSeconds();
+	clock.restart(); //reinicia o relógio para guardar o intervalo de tempo...
+
+	Imagem* img = mapImagens[estadoAtual];
+
+	sf::Vector2f tamCorpo = corpo->getSize();
+	sf::Vector2f escala = img->getEscala();
+
+	img->atualizar(indoParaEsq, dt);
+	corpo->setTextureRect(img->getRetang()); //usando o intRect aqui! 
+	corpo->setTexture(&(img->getTextura()));
+	corpo->setScale(escala.x, escala.y);
+
+}
+
+void Animacao::addAnimacao(const char* caminhoTextura, std::string nomeAnimacao, const int qtdImg, const float frame_duration, const sf::Vector2f scale)
+{
+	Imagem* img = new Imagem(caminhoTextura, qtdImg, frame_duration, scale);
+	mapImagens.insert({ nomeAnimacao, img });
+}
