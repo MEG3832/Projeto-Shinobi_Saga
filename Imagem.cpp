696baf7d00e991c0d538c9af464c1728a964bfec
Rachel Previtali <rachel_previtali@outlook.com>
2025-10-22 17:37:36 -0300

    Criação da classe Imagem

---
+++ Imagem.cpp
@@ -0,0 +1,59 @@
+#include "Imagem.h"
+
+Imagem::Imagem(const char* caminhoTextura, const int qtd_Img, const float frame_duration, const sf::Vector2f scale) :
+	pGraf(pGraf->getGerenciadorGrafico()),
+	textura(pGraf->carregarTextura(caminhoTextura)),
+	retangulo(0, 0, 0, 0),
+	escala(scale),
+	qnt_Imagens(qtd_Img),
+	imag_Atual(0),
+	duracao_Frame(frame_duration),
+	tempo_Total(0.0)
+{
+	retangulo.height = (float) textura.getSize().y; // "altura" da nossa textura
+	retangulo.width = (float) (textura.getSize().x) / qnt_Imagens; // largura de cada imagem do spritesheet
+}
+
+Imagem::~Imagem() { }
+
+void Imagem::atualizar(const bool indoParaEsq, const float dt)
+{
+	tempo_Total += dt; //guardamos o tempo total para saber se já deu o tempo que queremos que a sprite rode
+
+	if (tempo_Total >= duracao_Frame)
+	{
+		tempo_Total = 0.0; //reinicia a contagem de tempo...
+		imag_Atual++;
+
+		if (imag_Atual >= qnt_Imagens)
+			imag_Atual = 0; //reinicia o spritesheet
+
+	}
+
+	if (indoParaEsq) //invertemos a imagem se estiver indo para a esquerda
+	{
+		retangulo.left = (imag_Atual + 1) * retangulo.width; // a "leitura" da imagem será feita da direita para a esquerda
+		retangulo.width = -abs(retangulo.width);
+
+	}
+
+	else
+	{
+		retangulo.left = imag_Atual * retangulo.width;
+		retangulo.width = abs(retangulo.width);
+	}
+
+}
+
+void Imagem::resetar()
+{
+	tempo_Total = 0;
+	imag_Atual = 0;
+}
+
+const sf::IntRect Imagem::getRetang() { return retangulo; }
+
+const sf::Texture Imagem::getTextura() { return textura; } // nota, aqui era pra ser um ponteiro
+
+const sf::Vector2f Imagem::getEscala() { return escala; }
+
