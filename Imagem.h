92ff21c9df80a33555873323130387ccc035dba0
Rachel Previtali <rachel_previtali@outlook.com>
2025-10-22 17:37:58 -0300

    Criação da classe Imagem

---
+++ Imagem.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Header.h"
+#include "Gerenciador_Grafico.h"
+
+
+class Imagem
+{
+
+	Gerenciadores::Gerenciador_Grafico* pGraf;
+	sf::Texture textura;
+	sf::IntRect retangulo; //a "forma" retangular que vamos colocar a nossa sprite (uma das imagens da spritesheet).
+	const sf::Vector2f escala;
+	const int qnt_Imagens;
+	int imag_Atual; //guarda o "índice" da imagem atual (0, 1, 2...)
+	const float duracao_Frame; //tempo em que a imagem "estática" fica em tela.
+	float tempo_Total;
+
+public:
+
+	Imagem(const char* caminhoTextura, const int qtd_Img, const float frame_duration, const sf::Vector2f scale);
+	~Imagem();
+
+
+	void atualizar(const bool indoParaEsq, const float dt);
+	void resetar();
+	const sf::IntRect getRetang();
+	const sf::Texture getTextura();
+	const sf::Vector2f getEscala();
+
+};
+
