ab42bbcd695cce9b63435ff75558b2a4cbacde98
MEG3832 <dudagracia11@gmail.com>
2025-11-25 21:54:15 -0300

    Adicionei mais referências

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/* Sites com as sprites utilizadas:
+* Itch.io.“Top free Game assets tagged 2D”. Disponível em: https://itch.io/game-assets/free/tag-2d (Fundos e obstáculos).
+* CraftPix.net, “2D & 3D Game Assets,” CraftPix.net. https://craftpix.net/ (Demais Entidades).
+*/
+
 #include "Jogo.h"
 
 int main() {
